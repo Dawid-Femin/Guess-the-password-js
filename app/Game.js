7558283ffb281a778dd5f18d63ad8cde7cddff65
Dawid Femin <w65311@student.wsiz.edu.pl>
2022-02-18 16:22:26 +0100

    Basic structure of Game

---
+++ app/Game.js
@@ -0,0 +1,56 @@
+class Game {
+
+    quotes = [
+        {
+          text: "pan tadeusz",
+          category: "Utwór literacki",
+        },
+        {
+          text: "janko muzykant",
+          category: "Utwór literacki",
+        },
+        {
+          text: "akademia pana kleksa",
+          category: "Film",
+        },
+        {
+          text: "ogniem i mieczem",
+          category: "Film",
+        },
+      ];
+
+    constructor({
+        lettersWrapper,
+        categoryWrapper,
+        wordWrapper,
+        outputWrapper,
+    }) {
+        this.lettersWrapper = lettersWrapper;
+        this.categoryWrapper = categoryWrapper;
+        this.wordWrapper = wordWrapper;
+        this.outputWrapper = outputWrapper;
+
+        const {text, category} = this.quotes[Math.floor(Math.random() * this.quotes.length)];
+        this.categoryWrapper.innerHTML = category;
+        console.log(category);
+    }
+
+    guess (letter) {
+        console.log(letter);
+    }
+
+
+    drawLetters() {
+        for (let i=0; i<26; i++) {
+            const label = (i+10).toString(36);
+            const button = document.createElement('button');
+            button.innerHTML = label;
+            button.addEventListener('click', () => this.guess(label));
+            this.lettersWrapper.appendChild(button);
+        }
+    }
+
+    start() {
+        this.drawLetters();
+    }
+}
